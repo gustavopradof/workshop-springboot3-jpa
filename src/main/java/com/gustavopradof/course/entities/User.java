70458bbfd3f8bd3dce9c45d632ce57973c379649
Gustavo Prado <gd.prado1985@gmail.com>
2024-01-02 23:40:08 -0300

    Order,Instant

--- src/main/java/com/gustavopradof/course/entities/User.java
+++ src/main/java/com/gustavopradof/course/entities/User.java
@@ -1,12 +1,15 @@
 package com.gustavopradof.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +24,9 @@ public class User implements Serializable{
 		private String email;
 		private String phone;
 		private String passsword;
+		
+		@OneToMany(mappedBy =  "client")
+		private List<Order> orders = new ArrayList<>();
 		
 		public User() {
 			
@@ -50,6 +56,7 @@ public class User implements Serializable{
 		public void setName(String name) {
 			this.name = name;
 		}
+		
 
 		public String getEmail() {
 			return email;
@@ -75,6 +82,10 @@ public class User implements Serializable{
 			this.passsword = passsword;
 		}
 
+		public List<Order> getOrders() {
+			return orders;
+		}
+		
 		@Override
 		public int hashCode() {
 			return Objects.hash(id);
@@ -91,5 +102,6 @@ public class User implements Serializable{
 			User other = (User) obj;
 			return Objects.equals(id, other.id);
 		}
-		
+
+	
 }
