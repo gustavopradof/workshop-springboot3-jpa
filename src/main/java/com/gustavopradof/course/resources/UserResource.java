be456f5e3f138e27bb753d15256a77b9d3cf0670
Gustavo Prado <gd.prado1985@gmail.com>
2024-01-02 21:00:14 -0300

    first commit

---
+++ src/main/java/com/gustavopradof/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.gustavopradof.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gustavopradof.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+		public ResponseEntity<User> findAll(){
+			User u = new User(1L, "maria", "maria@gmail.com", "10", "12345");
+			return ResponseEntity.ok().body(u);
+		}
+}
