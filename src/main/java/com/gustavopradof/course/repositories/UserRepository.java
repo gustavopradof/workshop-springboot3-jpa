77bae70959247c363603dce40f8c938d7919b387
Gustavo Prado <gd.prado1985@gmail.com>
2024-01-02 22:42:15 -0300

    JPA Repository

---
+++ src/main/java/com/gustavopradof/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.gustavopradof.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gustavopradof.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
