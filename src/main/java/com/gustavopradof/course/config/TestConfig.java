d9165c15e146c919d6a24d4f28951609b477182e
Gustavo Prado <gd.prado1985@gmail.com>
2024-01-04 00:03:55 -0300

    Category

--- src/main/java/com/gustavopradof/course/config/TestConfig.java
+++ src/main/java/com/gustavopradof/course/config/TestConfig.java
@@ -1,7 +1,7 @@
 package com.gustavopradof.course.config;
 
 import java.time.Instant;
-import java.util.ArrayList;
+
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +9,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.gustavopradof.course.entities.Category;
 import com.gustavopradof.course.entities.Order;
 import com.gustavopradof.course.entities.User;
 import com.gustavopradof.course.entities.enums.OrderStatus;
+import com.gustavopradof.course.repositories.CategoryRepository;
 import com.gustavopradof.course.repositories.OrderRepository;
 import com.gustavopradof.course.repositories.UserRepository;
 
@@ -24,10 +26,19 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		
+		Category cat1 = new Category(null, "Electronics"); 
+		Category cat2 = new Category(null, "Books"); 
+		Category cat3 = new Category(null, "Computers"); 
+		
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
